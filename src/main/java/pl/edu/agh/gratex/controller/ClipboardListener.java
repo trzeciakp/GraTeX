4b58ed5db5ed55483ad3b9911580d33977eb4629
Łukasz Opioła <ziemmniaq@gmail.com>
2013-12-27 15:44:40 +0100

    Remove ClipboardXXX classes - copying logic is handled by DummySubgraph and CreationRemovalOperation.

--- src/main/java/pl/edu/agh/gratex/controller/ClipboardListener.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.agh.gratex.controller;
-
-/**
- *
- */
-public interface ClipboardListener {
-    public void setCopyingEnfabled(boolean copyingEnabled);
-    public void setPastingEfnabled(boolean pastingEnabled);
-}
