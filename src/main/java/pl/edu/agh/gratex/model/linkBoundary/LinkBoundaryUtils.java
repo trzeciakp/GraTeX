1d057eac07f5506dd4b08628be65c1766bb167ec
Łukasz Opioła <ziemmniaq@gmail.com>
2014-01-12 17:54:48 +0100

    Ctrl  with links snaps to horizontal / vertical

--- src/main/java/pl/edu/agh/gratex/model/linkBoundary/LinkBoundaryUtils.java
+++ src/main/java/pl/edu/agh/gratex/model/linkBoundary/LinkBoundaryUtils.java
@@ -5,6 +5,7 @@ import pl.edu.agh.gratex.model.boundary.Boundary;
 import pl.edu.agh.gratex.model.properties.ArrowType;
 
 import java.awt.*;
+import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 
 public class LinkBoundaryUtils {
@@ -32,8 +33,8 @@ public class LinkBoundaryUtils {
 
     public static Point getExitPointFromBoundary(Boundary boundary, double angle) {
         double r = Math.sqrt(boundary.getWidth() * boundary.getWidth() + boundary.getHeight() * boundary.getHeight()) / 2.0;
-        double x = Math.sin(Math.toRadians(angle)) * r;
-        double y = -Math.cos(Math.toRadians(angle)) * r;
+        double x = Math.sin(angle) * r;
+        double y = -Math.cos(angle) * r;
 
         double bottomRightX = (boundary.getWidth() + boundary.getLineWidth()) / 2.0;
         double bottomRightY = (boundary.getHeight() + boundary.getLineWidth()) / 2.0;
@@ -96,24 +97,23 @@ public class LinkBoundaryUtils {
     }
 
     public static double getAngleFromCursorLocation(Boundary owner, int mouseX, int mouseY) {
-        int middleX = owner.getTopLeftX() + owner.getWidth() / 2;
-        int middleY = owner.getTopLeftY() + owner.getHeight() / 2;
-        Point2D p1 = new Point(middleX, middleY);
-        Point2D p2 = new Point(mouseX, mouseY);
-        double angle = Math.toDegrees(Math.asin((mouseX - middleX) / p1.distance(p2)));
+        double middleX = owner.getTopLeftX() + owner.getWidth() / 2;
+        double middleY = owner.getTopLeftY() + owner.getHeight() / 2;
+        if (mouseX == middleX && mouseY == middleY) {
+            return 0;
+        }
+        double angle = Math.asin((mouseX - middleX) / Point.distance(middleX, middleY, mouseX, mouseY));
         if (mouseY < middleY) {
             if (mouseX < middleX) {
-                angle += 360;
+                angle += Math.PI * 2;
             }
         } else {
-            angle = 180 - angle;
+            angle = Math.PI - angle;
         }
         return angle;
     }
 
     public static double getAngleFromLineIntersection(Boundary boundary, int startX, int startY, int endX, int endY) {
-        int dx = endX - startX;
-        int dy = endY - startY;
         int closestX = boundary.getTopLeftX();
         int closestY = boundary.getTopLeftY();
         if (Math.abs(startX - boundary.getTopLeftX() - boundary.getWidth()) < Math.abs(startX - boundary.getTopLeftX())) {
@@ -122,18 +122,10 @@ public class LinkBoundaryUtils {
         if (Math.abs(startY - boundary.getTopLeftY() - boundary.getHeight()) < Math.abs(startY - boundary.getTopLeftY())) {
             closestY = boundary.getTopLeftY() + boundary.getHeight();
         }
-
-        if (dx == 0) {
+        if (startX == endX) {
             return getAngleFromCursorLocation(boundary, endX, closestY);
-        }
-        else if (dy == 0) {
-            return getAngleFromCursorLocation(boundary, closestX, endY);
         } else {
-            int borderDist = Math.abs(endX - closestX);
-            if (Math.abs(startY - closestY) < Math.abs(startX - closestX)) {
-                borderDist = Math.abs(endY - closestY);
-            }
-            return getAngleFromCursorLocation(boundary, endX - (int) (Math.signum(endX - startX) * borderDist), endY - (int) (Math.signum(endY - startY) * borderDist));
+            return getAngleFromCursorLocation(boundary, closestX, endY);
         }
     }
 }
